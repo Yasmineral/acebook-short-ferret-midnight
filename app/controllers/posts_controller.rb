144affe77134b8b78a9791dcf48dc6cf58f8e072
Jay Issuree <Jay_Issuree@hotmail.co.uk>
2019-11-12 10:26:49 +0000

    Moved methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,15 +17,15 @@ class PostsController < ApplicationController
     end
   end
 
-  def upvote
-    @post = Post.find(params[:id])
-    @post.upvote_by current_user
-    redirect_to posts_url
-  end
-
   def downvote
     @post = Post.find(params[:id])
     @post.downvote_by current_user
+    redirect_to posts_url
+  end
+
+  def upvote
+    @post = Post.find(params[:id])
+    @post.upvote_by current_user
     redirect_to posts_url
   end
 
